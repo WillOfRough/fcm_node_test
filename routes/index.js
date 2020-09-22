ee30fa963d53e57bee83a5c35492281bcaa1d113
jhhwang <9guyu77@naver.com>
2020-09-22 22:19:46 +0900

    web-push fcm db저장 매핑

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,111 @@ var express = require('express');
 const webpush = require('web-push');
 var bodyParser = require('body-parser');
 const firebaseAdmin = require('firebase-admin');
+const mysql = require('mysql');
 var path = require('path');
 var router = express.Router();
+const async = require('async');
+const fetch = require("node-fetch");
 
 var serviceAccount = require("../config/serviceAccount.json");
 firebaseAdmin.initializeApp({
-  credential : firebaseAdmin.credential.applicationDefault(serviceAccount)
+  credential : firebaseAdmin.credential.cert(serviceAccount)
+});
+
+router.post('/fcm_sub',(req,res)=>{
+
+  const mysqlConnection = mysql.createConnection({
+    host: 'hancom-test.c0d1bhka4gti.ap-northeast-2.rds.amazonaws.com', //db ip address
+    port : 3306, //db port number
+    user : 'admin', //db id
+    password : '12345678', //db password
+    database:'firebaseDB' //db schema name
+  });
+  let body = req.body;
+  let email = body.email;
+  let token = body.token;
+  let insert_query;
+  let fcm_delete_query;
+  console.log("email : "+email);
+  console.log("token : "+token);
+  mysqlConnection.connect(function (err) {
+    if (err) {
+      console.error('mysql connection error');
+      console.error(err);
+    } else {
+      let select_query = 'SELECT * FROM firebaseDB.User WHERE token = ?;'
+      mysqlConnection.query(select_query,token,(err1,result,fields)=>{
+        if(err1){
+          console.log('mysqlConnection : '+err1);
+        }else{
+          let delete_query = 'SELECT * FROM firebaseDB.User WHERE email = ?';
+          mysqlConnection.query(delete_query, email, (err1, result, fields) => {
+            if (err1) {
+              console.log('mysqlConnection : ' + err1);
+            } else {
+              console.log("result.length : " + result.length);
+              async.forEach(result, function (users,callback) {
+                console.log("users : "+users);
+                url = 'https://iid.googleapis.com/iid/info/'+users.token+'?details=true'
+                console.log("url : "+url);
+                fetch(url,{
+                  method:'GET',
+                  headers:{
+                    Authorization : "key = AAAA7GaJukU:APA91bGncEU4VLQbCGyH-rMRECahlp2kZj844q_GIzxpqg58AHEPMQhYJwwweHnl9HbcOkW3bWMDp0s2YIYNhx4dI3NxyxgJDChT6QxSBC3U4461nF_r3ROsRiSrPO1u31rW1GS-KMiK",
+                    'content-type':'application/json'
+                  }
+                }).then((res) => {
+                  if (res.error){
+                    fcm_delete_query = 'DELETE FROM firebaseDB.User WHERE token = ?';
+                    mysqlConnection.query(fcm_delete_query,users.token,(err1,result,fields)=>{
+                      if(err1){
+                        console.log('fcm_delete_query : '+err1);
+                      }else{
+                        console.log('fcm_delete_query finish');
+                      }
+                    })
+                  } else { // 실패를 알리는 HTTP 상태 코드면
+                    console.error(res.statusText);
+                  }
+                }).catch(err => console.error(err));
+              });
+            }
+          })
+          if(result.length>0) {
+            let delete_query = 'DELETE FROM firebaseDB.User WHERE token = ?';
+            mysqlConnection.query(delete_query, token, (err1, result, fields) => {
+              if (err1) {
+                console.log('mysqlConnection : ' + err1);
+              } else {
+                insert_query = 'INSERT INTO firebaseDB.User(email,token,server_num) VALUES (?,?,?)';
+                mysqlConnection.query(insert_query, [email, token, 1], (err1, result, fields) => {
+                  if (err1) {
+                    console.log('mysqlConnection : ' + err1);
+                  } else {
+                    console.log('mysqlConnection finish');
+                  }
+                })
+              }
+            })
+          }
+          else{
+            insert_query = 'INSERT INTO firebaseDB.User(email,token,server_num) VALUES (?,?,?);';
+            mysqlConnection.query(insert_query,[email,token,1],(err1,result,fields)=>{
+              if(err1){
+                console.log('mysqlConnection : '+err1);
+              }else{
+                console.log('mysqlConnection finish');
+              }
+            })
+          }
+          console.log('mysqlConnection finish');
+        }
+      });
+
+    }
+  });
+
+  res.status(201).json({});
 });
 
 router.get('/fcm_send',(req,res) =>{
